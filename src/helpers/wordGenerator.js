952f78a6df518821894a675262c76fd98ff2d29c
Michał Wrzosek <michal@wrzosek.pl>
2018-08-04 20:44:33 +0200

    small word generator improvement

--- src/helpers/wordGenerator.js
+++ src/helpers/wordGenerator.js
@@ -28,17 +28,23 @@ const randomSoft = () => soft[randomNumberFromRange(0, soft.length - 1)];
 const randomHard = () => hard[randomNumberFromRange(0, hard.length - 1)];
 
 const generate = () => {
-  const length = randomNumberFromRange(2, 7);
+  const length = randomNumberFromRange(4, 9);
 
   let word = '';
+  let softCount = 0;
   let hardCount = 0;
 
   for (let i = 0; i < length; i++) {
-    if (hardCount === 2 || Math.random() > 0.5) {
+    if (
+      softCount !== 2 &&
+      (hardCount === 2 || Math.random() > 0.5)
+    ) {
       word += randomSoft();
+      softCount++;
       hardCount = 0;
     } else {
       word += randomHard();
+      softCount = 0;
       hardCount++;
     }
   }
